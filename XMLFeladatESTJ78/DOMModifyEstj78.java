45265b350c76aba2befe15118535940b466e0716
Laszlo Levente <laszlolevi@icloud.com>
2020-12-03 22:26:24 +0100

    hibak javitasa

--- XMLFeladatESTJ78/DOMModifyEstj78.java
+++ XMLFeladatESTJ78/DOMModifyEstj78.java
@@ -14,10 +14,13 @@ public class DOMModifyEstj78 {
 			System.out.println(root.getNodeType());
 			NodeList els = root.getChildNodes();
 			for (int j = 0; j < els.getLength(); j++) {
+				// Vegigmegy a root gyerekein
 				if (els.item(j).getNodeName() == "novenyek") {
 					for (int i = 0; i < els.item(j).getChildNodes().getLength(); i++) {
+						// Megtalalta a novenyeket most annak gyerekein megy vegig
 						if (els.item(j).getChildNodes().item(i).getNodeName() == "noveny") {
 							for (int k = 0; k < els.item(j).getChildNodes().item(i).getChildNodes().getLength(); k++) {
+								// Noveny gyerekeinek megkeresi az arakat majd modositja 200-al
 								if (els.item(j).getChildNodes().item(i).getChildNodes().item(k).getNodeName() == "ar") {
 									int ar = Integer.parseInt(els.item(j).getChildNodes().item(i).getChildNodes()
 											.item(k).getTextContent());
--- XMLFeladatESTJ78/DOMReadEstj78.java
+++ XMLFeladatESTJ78/DOMReadEstj78.java
@@ -1,10 +1,21 @@
 package hu.domparse.estj78;
 
+import java.io.File;
+
 import javax.xml.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -12,41 +23,61 @@ import hu.domparse.estj78.DOMModifyEstj78;;
 
 public class DOMReadEstj78 {
 	private static void printNode(Node r) {
-		NodeList els = r.getChildNodes();
-		for (int j = 0; j < els.getLength(); j++) {
-			if (els.item(j).getNodeName() != "#text") {
-				Node n = els.item(j);
-				System.out.println(n.getNodeName());
-				if (n.hasChildNodes()) {
-					for (int i = 0; i < n.getChildNodes().getLength(); i++) {
-						Node s = n.getChildNodes().item(i);
-						if (s.getNodeType() == Node.ELEMENT_NODE) {
-							System.out.println("\t" + s.getNodeName() + ": " + s.getTextContent());
-						}
-					}
-
+		// Node nevének kiírása
+		if (r.getNodeName() != "#text") {
+			System.out.println(r.getNodeName());
+		}
+		// Gyerekek Node listába helyezése
+		NodeList children = r.getChildNodes();
+		for (int i = 0; i < children.getLength(); i++) {
+			Node child = children.item(i);
+			boolean isComplex = child.getTextContent().contains("\n");
+			// Attribute kiírása
+			if (child.hasAttributes()) {
+				NamedNodeMap attributes = child.getAttributes();
+				int numAttrs = attributes.getLength();
+				for (int j = 0; j < numAttrs; j++) {
+					Attr attr = (Attr) attributes.item(j);
+					String attrName = attr.getNodeName();
+					String attrValue = attr.getNodeValue();
+					System.out.println(" " + attrName + " : " + attrValue);
 				}
-
 			}
-
+			// Név és tartalom kíírása
+			if (isComplex) {
+				printNode(child);
+			} else {
+				System.out.print(" " + child.getNodeName());
+				System.out.println(": " + child.getTextContent());
+			}
 		}
 	}
 
 	public static void main(String[] args) {
 		try {
 
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder db;
-			db = dbf.newDocumentBuilder();
-			Document document = db.parse("webaruhaz.xml");
-			// root data
-			Element root = document.getDocumentElement();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dbBuilder.parse("webaruhaz.xml");
+			String filepath = "./webaruhaz.xml";
+			doc.getDocumentElement().normalize();
+
+			// Xpath
+			XPath xPath = XPathFactory.newInstance().newXPath();
+
+			Element root = doc.getDocumentElement();
 			printNode(root);
 			DOMModifyEstj78.modifyPrices(root);
 			printNode(root);
+			// Modositott xml mentese
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filepath));
+			transformer.transform(source, result);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
